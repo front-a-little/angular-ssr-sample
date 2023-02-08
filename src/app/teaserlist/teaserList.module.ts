4f98b2f0152ef4f13db339bcaf3733deff5f5b02
Patrick Lasota <patrick.lasota@basf.com>
2023-02-08 15:31:47 +0100

    showcase for materials custom themes

--- src/app/teaserlist/teaserList.module.ts
+++ src/app/teaserlist/teaserList.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { TeaserComponent } from "./teaser/teaser.component";
 import { ButtonComponent } from "../core/button/button.component";
 import { TeaserListComponent } from './teaser-list/teaser-list.component';
@@ -8,7 +9,8 @@ import { TeaserListComponent } from './teaser-list/teaser-list.component';
 @NgModule({
   declarations: [TeaserComponent, ButtonComponent, TeaserListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatProgressSpinnerModule
   ],
   exports: [
     TeaserListComponent
