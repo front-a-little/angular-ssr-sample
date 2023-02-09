2867aacf627daa9009bd03aa0fdceb6cb4c1f93c
Patrick Lasota <patrick.lasota@basf.com>
2023-02-09 17:05:52 +0100

    Routing showcase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TeaserListComponent} from "./teaserlist/teaser-list/teaser-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TeaserListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
